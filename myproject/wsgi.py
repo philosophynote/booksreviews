ffcaa772ece9e4c6f0e7eb89cfc37f5540a6683f
Naoki Takahashi <adverdest@gmail.com>
2021-09-11 11:09:02 +0900

    first commit

---
+++ myproject/wsgi.py
@@ -0,0 +1,9 @@
+import os
+
+from dj_static import Cling 
+from django.core.wsgi import get_wsgi_application
+#from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+application = Cling(get_wsgi_application())
