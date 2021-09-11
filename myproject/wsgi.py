a4b338201caadbf3c1428b24a3904dfb3e64dc62
Naoki Takahashi <adverdest@gmail.com>
2021-09-11 11:35:31 +0900

    Revert "first commit"
    This reverts commit ffcaa772ece9e4c6f0e7eb89cfc37f5540a6683f.

--- myproject/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dj_static import Cling 
-from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-
-application = Cling(get_wsgi_application())
--- sitemanage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SitemanageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sitemanage'
