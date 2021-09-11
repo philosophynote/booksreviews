699b9ef76a8bdc32595c1dd844fcde1f05e46b7d
Naoki Takahashi <adverdest@gmail.com>
2021-09-11 14:26:00 +0900

    500エラー

--- myproject/settings.py
+++ myproject/settings.py
@@ -127,7 +127,7 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FOWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['booksreviews-test.herokuapp.com','127.0.0.1']
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
